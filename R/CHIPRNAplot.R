58a41970796a77e78be8b36007ab7b28c79a4119
Erica Liu <ericaenjoy3@yahoo.co.uk>
2017-10-25 16:01:05 -0400

    Correcting for a bug

--- R/CHIPRNAplot.R
+++ R/CHIPRNAplot.R
@@ -108,20 +108,18 @@ setMethod(f = "plotBox",
       stop("option is out of bound (1:3)")
     }
     ldat <- melt(data.table(grp = grp, tpm), id.vars = "grp")
+    browser()
     theme_set(theme_grey(base_size = 15))
-    p1 <- ggplot(ldat, aes(~variable, ~value, color = ~grp)) +
-    #geom_jitter(alpha=I(1/4), aes(color=grp),na.rm=TRUE) +
-    # geom_violin(alpha=I(1/4), aes(fill=grp),na.rm=TRUE) +
-    geom_boxplot(na.rm = TRUE, notch = FALSE) +
-    labs(x = "",y = ylab)+
-    theme(legend.title = element_blank(), panel.spacing = unit(2, "lines"), legend.position = "top", axis.text.x = element_text(angle = 90))
-    p2 <- ggplot(ldat, aes(~grp, ~value, color = ~variable)) +
-    #geom_jitter(alpha=I(1/4), aes(color=grp),na.rm=TRUE) +
-    # geom_violin(alpha=I(1/4), aes(fill=grp),na.rm=TRUE) +
-    geom_boxplot(na.rm = TRUE, notch = FALSE) +
-    labs(x = "",y = ylab)+
-    theme(legend.title = element_blank(), panel.spacing = unit(2, "lines"), legend.position = "top", axis.text.x = element_text(angle = 90))
-    # png(pngfout,width=min(3000,length(unique(ldat$variable))*800),height=3000,res=300)
+    p1 <- ggplot(ldat, aes_(x = ~variable, y = ~value, color = ~grp)) +
+      geom_boxplot(na.rm = TRUE, notch = FALSE) +
+      labs(x = "",y = ylab)+
+      theme(legend.title = element_blank(), panel.spacing = unit(2, "lines"), legend.position = "top",
+        axis.text.x = element_text(angle = 90))
+    p2 <- ggplot(ldat, aes_(x = ~grp, y = ~value, color = ~variable)) +
+      geom_boxplot(na.rm = TRUE, notch = FALSE) +
+      labs(x = "",y = ylab)+
+      theme(legend.title = element_blank(), panel.spacing = unit(2, "lines"), legend.position = "top",
+        axis.text.x = element_text(angle = 90))
     pdf(pdffout, height = 7*2)
     multiplot(p1,p2,cols = 1)
     dev.off()
@@ -133,14 +131,14 @@ setMethod(f = "plotLimma",
   signature = c(limma.obj = "limma"),
   definition = function(limma.obj, pdffout) {
     lim <- max(abs(range(limma.obj@dat$pct)))
-    p1 <- ggplot(limma.obj@dat, aes(x = ~cmp, y = ~pct, fill = ~direction))+
+    p1 <- ggplot(limma.obj@dat, aes_(x = ~cmp, y = ~pct, fill = ~direction))+
     geom_bar(stat="identity") +
     labs(x="",y="Peak With Nearby DiffGenes (%)") +
     theme(legend.position='none') +
     scale_y_continuous(limits = c(-lim, lim),
       breaks = seq(-lim, lim, by = 10),
       labels = abs(seq(-lim, lim, by = 10))) +
-    geom_text(aes(x = ~cmp, y = ~pct, label = abs(~pct)),
+    geom_text(aes_(x = ~cmp, y = ~pct, label = abs(~pct)),
       hjust = 0.5, vjust = 0, size = 4, data = limma.obj@dat, inherit.aes = FALSE) +
     coord_flip()
     if (length(unique(as.character(limma.obj@dat$clus)))>1) {
@@ -159,11 +157,11 @@ setMethod(f="plotBar",
     tpm.cat <- apply(tpm.obj@tpm.val, 2, function(vec)factor(cut(vec, breaks = c(-Inf,1e-10,5,Inf), right = TRUE, labels = c("0", "<5 TPM", ">5 TPM"))))
     cnt.pct <- melt(data.table(grp = tpm.obj@grp.before, tpm.cat), id.var="grp") %>% group_by(.data$grp, .data$variable, .data$value) %>% tally() %>% group_by(.data$grp) %>% mutate(pct=(100*.data$n)/sum(.data$n))
     theme_set(theme_grey(base_size = 15))
-    p1 <- ggplot(cnt.pct, aes(x = ~variable, fill = ~value,y = ~n))+ geom_bar(stat = "identity") +
+    p1 <- ggplot(cnt.pct, aes_(x = ~variable, fill = ~value,y = ~n))+ geom_bar(stat = "identity") +
       xlab("") + ylab("Count") + facet_grid(. ~ grp) +
       theme(legend.title = element_blank(),panel.spacing = unit(2, "lines"),
         legend.position = "top", axis.text.x = element_text(angle=90))
-    p2 <- ggplot(cnt.pct, aes(x = ~variable,fill = ~value,y = ~pct)) + geom_bar(stat = "identity") +
+    p2 <- ggplot(cnt.pct, aes_(x = ~variable,fill = ~value,y = ~pct)) + geom_bar(stat = "identity") +
       xlab("") + ylab("%") + facet_grid(. ~ grp) +
       theme(legend.title = element_blank(), panel.spacing=unit(2, "lines"), legend.position="top",
         axis.text.x = element_text(angle = 90))
